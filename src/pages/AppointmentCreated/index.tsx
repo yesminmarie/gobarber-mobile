949207573562875be875368db5929927ab509aee
yesmin <ymslahoud@gmail.com>
2020-09-25 19:29:33 -0300

    formatting the date

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,5 +1,7 @@
-import { useNavigation } from '@react-navigation/native';
-import React, { useCallback } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import React, { useCallback, useMemo } from 'react';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import Icon from 'react-native-vector-icons/Feather';
 
 import {
@@ -10,8 +12,15 @@ import {
   OkButtonText,
 } from './styles';
 
+interface RouteParams {
+  date: number; // pega o valor date.getTime()
+}
+
 const AppointmentCreated: React.FC = () => {
   const { reset } = useNavigation();
+  const { params } = useRoute();
+
+  const routeParams = params as RouteParams;
 
   const handleOkPressed = useCallback(() => {
     // o usuário não consegue mais voltar para a tela de agendamento concluído
@@ -20,12 +29,22 @@ const AppointmentCreated: React.FC = () => {
       index: 0,
     });
   }, [reset]);
+
+  // EEEE -> dia da semana por extenso
+  const formattedDate = useMemo(() => {
+    return format(
+      routeParams.date,
+      "EEEE', dia' dd 'de' MMMM 'de' yyyy 'às' HH:mm'h'",
+      { locale: ptBR },
+    );
+  }, [routeParams.date]);
+
   return (
     <Container>
       <Icon name="check" size={80} color="#04d361" />
 
       <Title>Agendamento concluído</Title>
-      <Description>Teste</Description>
+      <Description>{formattedDate}</Description>
 
       <OkButton onPress={handleOkPressed}>
         <OkButtonText>Ok</OkButtonText>
